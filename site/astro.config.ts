a9f3a40c60810464d6004a2c92582afe4d9bbb9e
Ozgur Gunes <o.gunes@gmail.com>
2025-09-11 00:12:46 +0300

    fix teaser, add dev server network access support

--- site/astro.config.ts
+++ site/astro.config.ts
@@ -1,14 +1,30 @@
+import os from 'os'
 import { defineConfig } from 'astro/config'
 import { chassis } from './src/libs/astro'
 import { getConfig } from './src/libs/config'
 import { algoliaPlugin } from './src/plugins/algolia-plugin'
 import { stackblitzPlugin } from './src/plugins/stackblitz-plugin'
 
+function getLocalIp() {
+  const interfaces = os.networkInterfaces()
+  for (const name of Object.keys(interfaces)) {
+    for (const iface of interfaces[name]!) {
+      if (iface.family === 'IPv4' && !iface.internal) {
+        return iface.address
+      }
+    }
+  }
+  return 'localhost'
+}
+const hasHostFlag = process.argv.includes('--host')
+
 const isDev = process.env.NODE_ENV === 'development'
 
 const site = isDev
-  ? // In development mode, use the local dev server.
-    'http://localhost:4323'
+  // In development mode, use the local dev server.
+  ? hasHostFlag
+    ? `http://${getLocalIp()}:4321`
+    : 'http://localhost:4321'
   : process.env.DEPLOY_PRIME_URL !== undefined
     ? // If deploying on Netlify, use the `DEPLOY_PRIME_URL` environment variable.
       process.env.DEPLOY_PRIME_URL
